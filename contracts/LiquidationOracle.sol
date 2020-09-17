f7056b6377c30c8d9d9af4a03a830e2997d17633
VFI <71346651+VearnFinance@users.noreply.github.com>
2020-09-17 06:53:43 -0700

    Update LiquidationOracle.sol

--- contracts/LiquidationOracle.sol
+++ contracts/LiquidationOracle.sol
@@ -167,7 +167,7 @@ library SafeERC20 {
     }
 }
 
-interface yERC20 {
+interface vERC20 {
   function getPricePerFullShare() external view returns (uint256);
 }
 
@@ -194,24 +194,24 @@ contract LiquidationOracle is ReentrancyGuard, Ownable {
   using SafeMath for uint256;
 
   address public constant DAI = address(0x6B175474E89094C44Da98b954EedeAC495271d0F);
-  address public constant yDAI = address(0x16de59092dAE5CcF4A1E6439D611fd0653f0Bd01);
+  address public constant vDAI = address(0x16de59092dAE5CcF4A1E6439D611fd0653f0Bd01);
 
   address public constant USDC = address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
-  address public constant yUSDC = address(0xd6aD7a6750A7593E092a9B218d66C0A814a3436e);
+  address public constant vUSDC = address(0xd6aD7a6750A7593E092a9B218d66C0A814a3436e);
 
   address public constant USDT = address(0xdAC17F958D2ee523a2206206994597C13D831ec7);
-  address public constant yUSDT = address(0x83f798e925BcD4017Eb265844FDDAbb448f1707D);
+  address public constant vUSDT = address(0x83f798e925BcD4017Eb265844FDDAbb448f1707D);
 
   address public constant TUSD = address(0x0000000000085d4780B73119b644AE5ecd22b376);
-  address public constant yTUSD = address(0x73a052500105205d34Daf004eAb301916DA8190f);
+  address public constant vTUSD = address(0x73a052500105205d34Daf004eAb301916DA8190f);
 
   address public constant SUSD = address(0x57Ab1ec28D129707052df4dF418D58a2D46d5f51);
-  address public constant ySUSD = address(0xF61718057901F84C4eEC4339EF8f0D86D2B45600);
+  address public constant vSUSD = address(0xF61718057901F84C4eEC4339EF8f0D86D2B45600);
 
   address public constant BUSD = address(0x4Fabb145d64652a948d72533023f6E7A623C7C53);
-  address public constant yBUSD = address(0x04bC0Ab673d88aE9dbC9DA2380cB6B79C4BCa9aE);
+  address public constant vBUSD = address(0x04bC0Ab673d88aE9dbC9DA2380cB6B79C4BCa9aE);
 
-  address public constant ySWAP = address(0x45F783CCE6B7FF23B2ab2D70e416cdb7D6055f51);
+  address public constant vSWAP = address(0x45F783CCE6B7FF23B2ab2D70e416cdb7D6055f51);
   address public constant sSWAP = address(0x3b12e1fBb468BEa80B492d635976809Bf950186C);
   address public constant bSWAP = address(0x79a8C46DeA5aDa233ABaFFD40F3A0A2B1e5A4F27);
 
@@ -254,13 +254,13 @@ contract LiquidationOracle is ReentrancyGuard, Ownable {
   }
   function getPool(address _token) public pure returns (address) {
     if (_token == DAI) {
-      return ySWAP;
+      return vSWAP;
     } else if (_token == USDC) {
-      return ySWAP;
+      return vSWAP;
     } else if (_token == USDT) {
-      return ySWAP;
+      return vSWAP;
     } else if (_token == TUSD) {
-      return ySWAP;
+      return vSWAP;
     } else if (_token == BUSD) {
       return bSWAP;
     } else if (_token == SUSD) {
