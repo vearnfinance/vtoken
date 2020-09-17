f43a367b0b20af0f09304df2da191d38a0ce4c7d
VFI <71346651+VearnFinance@users.noreply.github.com>
2020-09-17 13:35:19 -0700

    Update and rename YTUSDv2.sol to VTUSDv2.sol

--- contracts/VTUSDv2.sol
+++ contracts/VTUSDv2.sol
@@ -365,7 +365,7 @@ interface LendingPoolAddressesProvider {
     function getLendingPoolCore() external view returns (address);
 }
 
-contract yTUSD is ERC20, ERC20Detailed, ReentrancyGuard, Ownable, Structs {
+contract vTUSD is ERC20, ERC20Detailed, ReentrancyGuard, Ownable, Structs {
   using SafeERC20 for IERC20;
   using Address for address;
   using SafeMath for uint256;
@@ -390,7 +390,7 @@ contract yTUSD is ERC20, ERC20Detailed, ReentrancyGuard, Ownable, Structs {
 
   Lender public provider = Lender.NONE;
 
-  constructor () public ERC20Detailed("iearn TUSD", "yTUSD", 18) {
+  constructor () public ERC20Detailed("vearn TUSD", "vTUSD", 18) {
     token = address(0x0000000000085d4780B73119b644AE5ecd22b376);
     apr = address(0xdD6d648C991f7d47454354f4Ef326b04025a48A8);
     dydx = address(0x1E0447b19BB6EcFdAe1e4AE1694b0C3659614e4e);
